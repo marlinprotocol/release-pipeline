6f7ead31d4c2c44f71450e2478eb20eb2780d0db
Roshan <thisisroshansmail@gmail.com>
2024-07-13 11:55:04 +0000

    Create dnsproxy-arm64.sh

---
+++ dnsproxy-arm64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# To be used inside a golang docker container for reproducible builds
+
+set -e
+
+cd /
+
+wget -O - https://musl.cc/aarch64-linux-musl-native.tgz | tar -xz
+
+cd /code
+
+CC=/aarch64-linux-musl-native/bin/aarch64-linux-musl-gcc go build -ldflags='-s -w -linkmode external -extldflags "-static"' -buildvcs=false
