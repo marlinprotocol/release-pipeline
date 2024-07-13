54551f8778d7728173292763efa76bf59899ed28
Roshan <thisisroshansmail@gmail.com>
2024-07-13 11:54:23 +0000

    Create dnsproxy-amd64.sh

---
+++ dnsproxy-amd64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# To be used inside a golang docker container for reproducible builds
+
+set -e
+
+cd /
+
+wget -O - https://musl.cc/x86_64-linux-musl-native.tgz | tar -xz
+
+cd /code
+
+CC=/x86_64-linux-musl-native/bin/x86_64-linux-musl-gcc go build -ldflags='-s -w -linkmode external -extldflags "-static"' -buildvcs=false
