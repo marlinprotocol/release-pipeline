bd73eb5a42639c7fec02f177d3beb477a0e180d5
Roshan <thisisroshansmail@gmail.com>
2024-07-15 11:39:18 +0000

    Update dnsproxy-amd64.sh

--- dnsproxy-amd64.sh
+++ dnsproxy-amd64.sh
@@ -10,4 +10,4 @@ wget -O - https://musl.cc/x86_64-linux-musl-native.tgz | tar -xz
 
 cd /code
 
-CC=/x86_64-linux-musl-native/bin/x86_64-linux-musl-gcc go build -ldflags='-buildid= -s -w -linkmode external -extldflags "-static"' -buildvcs=false -trimpath
+CC=/x86_64-linux-musl-native/bin/x86_64-linux-musl-gcc go build -ldflags='-buildid= -s -w -linkmode external -extldflags "-static"' -buildvcs=false
